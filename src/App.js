256df2eed5647a23ed53a06861286f1c3cd1f5fd
Rakib ul banna <rakibulbanna@gmail.com>
2023-11-04 05:18:01 +0600

    Done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import './App.css';
 
 function App() {
-  const [images, setImages] = useState([
+  const [gallery, setGallery] = useState([
     { id: 1, url: "image-1.webp" },
     { id: 2, url: "image-2.webp", },
     { id: 3, url: "image-3.webp", },
@@ -19,7 +19,10 @@ function App() {
   const [selectedImages, setSelectedImages] = useState([]);
   const [draggedImage, setDraggedImage] = useState(null);
 
+  const selectedImgeLength = selectedImages.length
+
   const handleImageClick = (selected, image) => {
+    // Filter those images which are not selected otherwise adding image id to selected image
     if (!selected) {
       setSelectedImages(selectedImages.filter((id) => id !== image.id));
     } else {
@@ -28,58 +31,83 @@ function App() {
   };
 
   const handleImageDragStart = (image) => {
+    // set dragging image to state to memorize
     setDraggedImage(image);
   };
 
   const handleImageDragOver = (image) => {
-    if (draggedImage === image) return;
-    const updatedImages = [...images];
-    const draggedIndex = images.indexOf(draggedImage);
-    const targetIndex = images.indexOf(image);
-    updatedImages.splice(draggedIndex, 1);
-    updatedImages.splice(targetIndex, 0, draggedImage);
+    if (draggedImage !== image) {
+      // Makeing a deep copy of gallery
+      const updatedImages = [...gallery];
 
-    setImages(updatedImages);
+      // Dragged image index finding
+      const draggedIndex = gallery.indexOf(draggedImage);
+
+      // Target position index finding to set dragged image
+      const targetIndex = gallery.indexOf(image);
+
+      // Deleting the dragged image from its previous position from gallery
+      updatedImages.splice(draggedIndex, 1);
+
+      // Inserting dragged image in taget Index
+      updatedImages.splice(targetIndex, 0, draggedImage);
+
+      // Update the gallery
+      setGallery(updatedImages);
+    }
   };
 
   const handleImageDrop = () => {
+    // After compleating dragging drag state make empty 
     setDraggedImage(null);
   };
 
 
   const handleDeleteSelectedImages = () => {
-    const updatedImages = images.filter((image) => !selectedImages.includes(image.id));
-    setImages(updatedImages);
+    // Filtering images which are not selected for selected image deletion 
+    const updatedImages = gallery.filter((image) => !selectedImages.includes(image.id));
+    setGallery(updatedImages);
     setSelectedImages([]);
   };
 
   return (
-    <div className="main-container">
-      <div className="controls">
-        <div>{selectedImages.length} File Selecetd </div>
-        <a onClick={handleDeleteSelectedImages}>
-          Delete Files
-        </a>
-      </div>
-      <div className="main-container image-gallery">
-        {images.map((image,index) => (
-          <div
-            key={image.id}
-            className={`${selectedImages.includes(image.id) ? "selected-image-container" : "image-container"} ${index == 0 ? 'large-image' : ''}`}
-            //   onClick={() => handleImageClick(image)}
-            onDragStart={() => handleImageDragStart(image)}
-            onDragOver={() => handleImageDragOver(image)}
-            onDrop={handleImageDrop}
-            draggable
+    <div className="mother-container">
+      <div className="main-container">
+        <div className="controls">
+          {
+            selectedImgeLength > 0 ?
+              <>
+                <div><input type="checkbox" checked /> {selectedImgeLength} File Selecetd </div>
+                <a onClick={handleDeleteSelectedImages}>
+                  Delete Files
+                </a>
+              </>
+              : <div className="txt-btm-border">Gallery </div>
+          }
 
-          >
-            
-              <img src={image.url} alt={`Image ${image.id}`} />
-              <input type="checkbox" onChange={(e) => handleImageClick(e.target.checked, image)} class="checkbox"></input>
-            
+        </div>
+        <div className="image-gallery">
+          {gallery.map((image, index) => (
+            <div
+              key={image.id}
+              className={`${selectedImages.includes(image.id) ?
+                "selected-image-container" :
+                "image-container"} ${index == 0 ? 'large-image' : ''}`}
+              onDragStart={() => handleImageDragStart(image)}
+              onDragOver={() => handleImageDragOver(image)}
+              onDrop={handleImageDrop}
+              draggable
+            >
+              <img src={image.url} alt={`Image ${image.url}`} />
+              <input className="checkbox" type="checkbox" onChange={(e) => handleImageClick(e.target.checked, image)} />
+
+            </div>
+          ))}
+          <div className="add-photo">
+            <img src="empty.png" />
+            Add Images
           </div>
-        ))}
-
+        </div>
       </div>
     </div>
   );
